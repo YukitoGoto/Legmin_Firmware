998c4a844945d3db030d38fc2f9bf03e81d796b1
yukito goto <ykt3693@outlook.jp>
2019-11-08 18:35:54 +0900

    add sample couldn't write to promicro

--- Pesticide_residue_LCD/src/main.cpp
+++ Pesticide_residue_LCD/src/main.cpp
@@ -1,10 +1,25 @@
+//#include <Wire.h>
+//#include <ST7032.h>
 #include <Arduino.h>
-#include <ST7032.h>
+
+//ST7032 lcd;
 
 void setup() {
-  // put your setup code here, to run once:
+  /*// LCD表示領域設定(8桁, 2行)
+  lcd.begin(8, 2);
+
+  // コントラスト設定(0〜63)
+  lcd.setContrast(30);
+*/
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // LCD表示(1行目)
+  /*lcd.setCursor(0, 0);
+  lcd.print("test");
+
+  // LCD表示(2行目)
+  lcd.setCursor(0, 1);
+  lcd.print("test");
+*/
 }
