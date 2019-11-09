a90dbda68bb14970943bbb02112aa92aa3f249d4
yukito goto <ykt3693@outlook.jp>
2019-11-09 13:44:47 +0900

    LCD test (PIOでの書き込み失敗→ArduinoIDEでは成功)

--- Pesticide_residue_LCD/src/main.cpp
+++ Pesticide_residue_LCD/src/main.cpp
@@ -1,25 +1,24 @@
-//#include <Wire.h>
-//#include <ST7032.h>
+#include <Wire.h>
+#include <ST7032.h>
 #include <Arduino.h>
 
-//ST7032 lcd;
+ST7032 lcd;
 
 void setup() {
-  /*// LCD表示領域設定(8桁, 2行)
+  //LCD表示領域設定(8桁, 2行)
   lcd.begin(8, 2);
 
-  // コントラスト設定(0〜63)
+  //コントラスト設定(0〜63)
   lcd.setContrast(30);
-*/
+
 }
 
 void loop() {
-  // LCD表示(1行目)
-  /*lcd.setCursor(0, 0);
+  //LCD表示(1行目)
+  lcd.setCursor(0, 0);
   lcd.print("test");
 
   // LCD表示(2行目)
   lcd.setCursor(0, 1);
   lcd.print("test");
-*/
 }
