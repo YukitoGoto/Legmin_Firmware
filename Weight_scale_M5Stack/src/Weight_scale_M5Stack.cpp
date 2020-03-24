85fa3b55f549498383fb808f341bb7b96db48213
yukito goto <ykt3693@outlook.jp>
2020-03-24 19:14:01 +0900

    status _report__2020_03_24

--- Weight_scale_M5Stack/src/Weight_scale_M5Stack.cpp
+++ Weight_scale_M5Stack/src/Weight_scale_M5Stack.cpp
@@ -1,71 +1,47 @@
 //開発版・デバック用
 #include "Weight_scale_M5Stack.h"
 
-#define Vout_pin 35     //analogpin
-#define Read_cnt_ 1000
+Weight_M5::Weight_M5(int Signal_Pin = Default_signal_pin,int Lcd_text_size = Default_lcd_text_size){
+  //諸変数の初期化
+  Signal_Pin_ = Signal_Pin,Lcd_text_size_ = Lcd_text_size;
+  Vout_ = 0.0,Weight_ = 0.0;
+  Lcdflag = false;
+}
 
-#define EEP_ADRS_OFF 0    //offset用のアドレス:0~3
-#define EEP_SIZE 4        //EEPROMで扱うデータのサイズ
-
-//個体毎に調整が必要なdefineを以下に示す。
-#define Analog_Max_Vout 3.30
-/*---Analog_Max_Vout:Analog値がAnalog_Maxの時の出力電圧[v] promicro:4.56[v] esp32:3.30[v]---*/
-
-#define Analog_Max 4095.0
-/*---Analog値がMaxの時の値 promicro:1024.0 esp32:4059.0---*/
-
-#define Dv_Dw 0.01780
-//ゴトウユキトが手動で調整
-/*---Dv_Dw:電圧を重さに変換する定数 promicro:0.02400 esp32:0.01780---*/
-
-typedef union{//前回起動時のデータを共用体に記憶しておく。
-  float all_data;
-  //この処理系ではfloatは4バイト
-  uint8_t part_data[4];
-}ROM;
-
-ROM offset;                   //offset:Vout_offset用の共用体
-
-float Vout,W;                 //Vout:出力電圧[v] W:重さ[kg]
-float Vout_offset;            //Vout_offset:体重計起動時のVoutの値(オフセット)
-float dv_dw;                  //dv_dw:グラフの傾き自動調整される予定だった。初期値DV_DWのまま。
-
-void Read_Vout(int Read_cnt);            //Read_Vout:出力電圧測定関数 Read_cnt:読み取り回数
-void Lead_W(float Vout_);                //Lead_W:出力電圧を重さに変換する関数 Vout:出力電圧[v]
-void Re_Vout_offset(void);               //Re_Vout_offset:オフセットを求める関数。求めた値をEEPROMに格納する。0[kg]調整に使用する。
-void write_data(int adr);                //write_data:EEPROMに4バイトごとに書き込む adr:アドレス
-void read_data(int adr);                 //read_data:EEPROMから4バイトごとに読み込む
-
-void setup() {
-  //Serial.begin(9600);
+void Weight_M5::begin(void) {
   //M5Stack初期化
   M5.begin();
-  M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextSize(Lcd_text_size_);
 
-  //esp32ではeepromの初期宣言が必要
+  //EEPROMの初期化 esp32ではeepromの初期宣言が必要
   EEPROM.begin(EEP_SIZE);
-
-  //諸変数の初期化
-  Vout = 0.0,W = 0.0;
-  dv_dw = Dv_Dw;
 
   //Vout_offsetの取得
   read_data(EEP_ADRS_OFF);
-  Vout_offset = offset.all_data;
+  Vout_offset_ = ROM.all_data;
 }
 
-void loop() {
+void Weight_M5::update_val(void) {
   //スイッチの状態を更新
   M5.update();
-  
-  //スイッチが押されたらRE_Vout_offsetを呼び出す。0[kg]調整される。
-  if(M5.BtnA.isPressed()){
-    Re_Vout_offset();
-    
-    //Serial.print("\n----------------------------------\n");
-    //Serial.print("Vout_offset[V] = ");  Serial.println(Vout_offset,4);
 
-    M5.Lcd.setCursor(0,0);
+  //スイッチが押されたらRe_Vout_offsetを呼び出す。0[kg]調整される。
+  if(M5.BtnA.isPressed())
+  {
+    Re_Vout_offset();
+    Lcdflag = true;
+  }
+  else{
+    Read_Vout(Sample_cnt_);
+    Lead_W(Vout_);
+    Lcdflag = false;
+  }
+}
+
+void Weight_M5::update_lcd(void){
+  M5.Lcd.setCursor(0,0);
+  if(Lcdflag){
+    Lcdflag = false;
     //1行目
     M5.Lcd.println("offset!");
     //2行目
@@ -74,81 +50,62 @@ void loop() {
     delay(1000);
   }
   else{
-    Read_Vout(Read_cnt_);
-    Lead_W(Vout);
-    //Serial.print("\n----------------------------------\n");
-    //Serial.print("Vout[V] = "); Serial.println(Vout,4);
-    //Serial.print("W[Kg] = "); Serial.println(W,4);
-    
+    Lcdflag = true;
     //1行目
-    M5.Lcd.setCursor(0,0);
     M5.Lcd.println("Nowvalue");
-
     //2行目
     M5.Lcd.print(" ");
-    if(W < 10.0){
-      if(W <= 5.0){
+    if(Weight_ < 10.0){
+      if(Weight_ <= 5.0){
       //led反転
       }
-      (W < 0) ? M5.Lcd.print(0.0,2) : M5.Lcd.print(W,2);
+      (Weight_ < 0) ? M5.Lcd.print(0.0,2) : M5.Lcd.print(Weight_,2);
     }
     else{
-      M5.Lcd.print(W,1);
+      M5.Lcd.print(Weight_,1);
     }
-    M5.Lcd.print("[L]");
-    
+    M5.Lcd.println("[L]");
+
     delay(200);
   }
 }
 
-void Read_Vout(int Read_cnt){
+void Weight_M5::Read_Vout(int Sample_cnt){
   int cnt = 0,Vout_val = 0;
   float Vout_sum = 0.0;
   
-  for(cnt = 0;cnt < Read_cnt;cnt++){
-    Vout_val = analogRead(Vout_pin);
-    Vout = (float)Vout_val * Analog_Max_Vout / Analog_Max;
-    Vout_sum += Vout;
+  for(cnt = 0;cnt < Sample_cnt;cnt++){
+    Vout_val = analogRead(Signal_Pin_);
+    Vout_ = (float)Vout_val * Analog_Max_Vout / Analog_Max;
+    Vout_sum += Vout_;
   }
   
-  Vout = Vout_sum / (float)Read_cnt;    //Read_cnt回測った電圧の平均値を採用する。
+  Vout_ = Vout_sum / (float)Sample_cnt;    //Sample_cnt回測った電圧の平均値を採用する。
 }
 
-void Lead_W(float Vout_){
-  W = (Vout_ - Vout_offset) / dv_dw;
+void Weight_M5::Lead_W(float Vout){
+  Weight_ = (Vout_ - Vout_offset_) / Dv_Dw;
 }
 
-void Re_Vout_offset(void){
-  Read_Vout(Read_cnt_);
-  Vout_offset = Vout;
-  offset.all_data = Vout;
+void Weight_M5::Re_Vout_offset(void){
+  Read_Vout(Sample_cnt_);
+  Vout_offset_ = Vout_;
+  ROM.all_data = Vout_;
   write_data(EEP_ADRS_OFF);
 }
 
-void write_data(int adr){
+void Weight_M5::write_data(int adr){
   int i;
-  for (i = 0;i < 4;i++){
-    if(adr == EEP_ADRS_OFF)  
-      EEPROM.write(adr + i,offset.part_data[i]);
-    else{
-      //Serial.print("\n----------------------------------\n");
-      //Serial.print("write_Error!\n");
-      break;
-    }
+  for (i = 0;i < EEP_SIZE;i++){
+    EEPROM.write(adr + i,ROM.part_data[i]);
     //eps32では書き込み後にコミットが必要
     EEPROM.commit();
   }
 }
 
-void read_data(int adr){
+void Weight_M5::read_data(int adr){
   int i;
-  for(i = 0;i < 4;i++){
-    if(adr == EEP_ADRS_OFF)
-      offset.part_data[i] = EEPROM.read(adr + i);
-    else{
-      //Serial.print("\n----------------------------------\n");
-      //Serial.print("read_Error!\n");
-      break;
-    }
+  for(i = 0;i < EEP_SIZE;i++){
+    ROM.part_data[i] = EEPROM.read(adr + i);
   }
 }
--- Weight_scale_M5Stack/src/Weight_scale_M5Stack.h
+++ Weight_scale_M5Stack/src/Weight_scale_M5Stack.h
@@ -5,47 +5,47 @@
 #include<EEPROM.h>
 #include<M5Stack.h>
 
-//調整不要な定数を以下に示す。
-#define EEP_ADRS_OFF 0    //offset用のアドレス:0~3
-#define EEP_SIZE 4        //EEPROMで扱うデータのサイズ
+//EEPROMに使う定数
+#define EEP_ADRS_OFF 0          //Vout_offset用のアドレス:0~3
+#define EEP_SIZE 4              //EEPROMで扱うデータのサイズ この処理系(Arduino)ではfloat型が4バイトとなる。
 
-//調整可能な定数を以下に示す。
-#define Analog_Max_Vout 3.30
-/*---Analog_Max_Vout:Analog値がAnalog_Maxの時の出力電圧[v] promicro:4.56[v] esp32:3.30[v]---*/
+//アナログ信号処理に使う定数
+#define Analog_Max_Vout 3.30    //Analog_Max_Vout:Analog値がAnalog_Maxの時の出力電圧[v] promicro:4.56[v] esp32:3.30[v]
+#define Analog_Max 4095.0       //Analog値がMaxの時の値 promicro:1023.0 esp32:4095.0
+#define Sample_cnt_ 1000        //アナログ信号のサンプリング回数
+#define Dv_Dw 0.01780           //ゴトウユキトが手動で調整 Dv_Dw:電圧を重さに変換する定数 promicro:0.02400 esp32:0.01780
 
-#define Analog_Max 4095.0
-/*---Analog値がMaxの時の値 promicro:1023.0 esp32:4095.0---*/
-
-#define Dv_Dw 0.01780
-//ゴトウユキトが手動で調整
-/*---Dv_Dw:電圧を重さに変換する定数 promicro:0.02400 esp32:0.01780---*/
+//ピンアサイン,LCD文字大きさ
+#define Default_signal_pin 35   //Signal_Pinのデフォルト値
+#define Default_lcd_text_size 4 //Lcd_text_sizeのデフォルト値
 
 class Weight_M5{
 public:
-    /*
-    Signal_Pin:アンプからのアナログ信号を読み取るピン(デフォルトでは35ピンが指定)
-    */
 
-    Weight_M5(int Signal_Pin);
+    Weight_M5(int Signal_Pin = Default_signal_pin,int Lcd_text_size = Default_lcd_text_size);//Signal_Pin:アンプからのアナログ信号を読み取るピン Lcd_text_size:LCDに表示するテキストのサイズ
 
-    void begin(void);
+    void begin(void);                       //begin:setup関数内で必ず実行すること! M5Stackの初期化やEEPROMの初期化を行う。
 
-    void update_val(void);
+    void update_val(void);                  //update_val:Weight_ Vout_offset_ 等の更新
 
-    void print_Weight(void);
+    void update_lcd(void);                  //update_lcd:LCDの内容を現在の値で更新
 
-    float Weight_;                          //W:重さ[kg]
-
+    float Weight_;                          //Weight_:重さ[kg]
 
 private:
+    bool Lcdflag;                            //Lcdflag:LCD制御に使用 
+
+    int Signal_Pin_;                         //Signal_Pin_:アンプからのアナログ信号を読み取るピン
+
+    int Lcd_text_size_;                      //Lcd_text_size_:LCDに表示するテキストのサイズ
 
     float Vout_;                             //Vout:出力電圧[v]
     
     float Vout_offset_;                      //Vout_offset:体重計起動時のVoutの値(オフセット)  
 
-    void Read_Vout(int Read_cnt);            //Read_Vout:出力電圧測定関数 Read_cnt:読み取り回数
+    void Read_Vout(int Sample_cnt);          //Read_Vout:出力電圧測定関数 Sample_cnt:読み取り回数
     
-    void Lead_W(float Vout_);                //Lead_W:出力電圧を重さに変換する関数 Vout:出力電圧[v]
+    void Lead_W(float Vout);                //Lead_W:出力電圧を重さに変換する関数 Vout:出力電圧[v]
     
     void Re_Vout_offset(void);               //Re_Vout_offset:オフセットを求める関数。求めた値をEEPROMに格納する。0[kg]調整に使用する。
     
@@ -53,11 +53,11 @@ private:
     
     void read_data(int adr);                 //read_data:EEPROMから4バイトごとに読み込む
     
-    typdef union{
+    union{                                   //ROM:Vout_offset用の共用体
         float all_data;
         uint8_t part_data[EEP_SIZE];
     }ROM;
 
-}
+};
 
 #endif //Weight_scale_M5Stack.h
