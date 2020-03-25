75356271437c087eb2664e389a7b114bd6ff8741
yukito goto <ykt3693@outlook.jp>
2020-03-25 19:33:47 +0900

    fix lib

--- lib/Pomp_control/src/Pomp_control.cpp
+++ lib/Pomp_control/src/Pomp_control.cpp
@@ -1,8 +1,10 @@
 #include "Pomp_control.h"
 
-Pomp_control::Pomp_control(void){
+Pomp_control::Pomp_control(int Pomp_pwmpin_,int Flowmeter_pin_,int Pressure_pin_){
+  //ピン設定
+  Pomp_pwmpin = Pomp_pwmpin_,Flowmeter_pin = Flowmeter_pin_,Pressure_pin = Pressure_pin_;
   //諸変数の初期化
-  shutoff = false,TargetLmp = 0.0,CurrentLmp = 0.0,CurrentPress = 0.0;
+  shutoff = false,TargetLpm = 0.0,CurrentLpm = 0.0,CurrentPress = 0.0;
   pulseCounter = 0.0,CurrentHz = 0.0,DT = 0.0,Vout = 0.0;
 }
 
@@ -26,14 +28,14 @@ void Pomp_control::update_power(void){
 }
 
 void Pomp_control::control_val(void){
-  //流量の入力値を0.0~Max_targetlmpに制限する。
-  (TargetLmp > Max_targetlmp) ? TargetLmp = Max_targetlmp : TargetLmp = TargetLmp;
-  (TargetLmp < 0.0) ? TargetLmp = 0.0 : TargetLmp = TargetLmp;
+  //流量の入力値を0.0~Max_targetLpmに制限する。
+  (TargetLpm > Max_targetLpm) ? TargetLpm = Max_targetLpm : TargetLpm = TargetLpm;
+  (TargetLpm < 0.0) ? TargetLpm = 0.0 : TargetLpm = TargetLpm;
   //各値の更新を行う。
   getCurrentLpm();
   Lead_P();
   //求めた値からPID制御して、操作量としてDT(デューティー比)を求める。ポンプ内のモーターへ反映。
-  DT = motor->update_val(TargetLmp,CurrentLmp,Control_interval);
+  DT = motor->update_val(TargetLpm,CurrentLpm,Control_interval);
   //シャットオフ圧力を超えていたら、DTを零にしてモーターを停止する。
   if(shutoff){
     DT = 0.0;
@@ -48,7 +50,7 @@ void Pomp_control::onRising(void) {
 
 void Pomp_control::getCurrentLpm(void) {
   CurrentHz = (double) pulseCounter * (1.0 / Control_interval);
-  CurrentLmp = (float) CurrentHz / DIV_NUMBER_FOR_LPM_FROM_HZ;
+  CurrentLpm = (float) CurrentHz / DIV_NUMBER_FOR_LPM_FROM_HZ;
   pulseCounter = 0;
 }
 
@@ -65,6 +67,6 @@ void Pomp_control::Lead_P(void){
   Vout = Vout_sum / (float)Sample_cnt;              //Sample_cnt回測った電圧の平均値を採用する。
   CurrentPress = (Vout - Vout_offset) / Dv_Dp;      //出力電圧から現在の圧力を求める。  
   //シャットオフ圧力を超えていたら有効にする。
-  if(CurrentLmp > Max_pomp_p)
+  if(CurrentLpm > Max_pomp_p)
     shutoff = true;
 }
