c04b68ae0085a160ba14cd530de64c7167831f5f
yukito goto <ykt3693@outlook.jp>
2020-08-18 17:44:51 +0900

    fix mac addr & add comment

--- PS4_Controller/example/get_mac_addr.cpp
+++ PS4_Controller/example/get_mac_addr.cpp
@@ -1,21 +1,20 @@
+//esp32のBluetooth通信には、Bluetoothのmacアドレスを使う必要がある！
 #include <Arduino.h>
 #include "esp_system.h"
 
-void setup() {
-    //baudrate = 9600
+void setup(void) {
     Serial.begin(9600);
 }
 
 void loop() {
-    // Get MAC address for WiFi station
-    uint8_t baseMac[6];
+    uint8_t btMac[6],baseMac[6];
+
+    Serial.println("-----------------------------");
+    esp_read_mac(btMac, ESP_MAC_BT);
+    Serial.printf("[Bluetooth] Mac Address = %02X:%02X:%02X:%02X:%02X:%02X\n", btMac[0], btMac[1], btMac[2], btMac[3], btMac[4], btMac[5]);
     esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
-    char baseMacChr[18] = {0};
-    sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+    Serial.printf("[WiFi] Mac Address = %02X:%02X:%02X:%02X:%02X:%02X\n", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
 
-    Serial.print("\nMAC: ");
-    Serial.println(baseMacChr);
-
-    //5秒ごとにMACアドレスを表示
-    delay(5000);
+    //10秒ごとにMACアドレスを表示
+    delay(10000);
 }
