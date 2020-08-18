60c700dd7df077da3327120ea29f1b7eb112154d
yukito goto <ykt3693@outlook.jp>
2020-08-18 17:07:33 +0900

    reset main.cpp

--- PS4_Controller/src/main.cpp
+++ PS4_Controller/src/main.cpp
@@ -1,12 +1,6 @@
 #include <Arduino.h>
 #include <PS4Controller.h>
 
-//#define GET_MAC_ADDR
-#ifdef GER_MAC_ADDR
-#include "esp_system.h"
-void get_mad_addr();
-#endif //GET_MAC_ADDR
-
 char esp32_mac_addr[] = "30:ae:a4:08:01:90";
 
 void setup() {
@@ -23,15 +17,3 @@ void loop() {
     }
     delay(3000);
 }
-
-#ifdef GET_MAC_ADDR
-void get_mac_addr() {
-  uint8_t baseMac[6];
-  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
-  char baseMacChr[18] = {0};
-  sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
-
-  Serial.print("\nMAC: ");
-  Serial.println(baseMacChr);
-}
-#endif //GET_MAC_ADDR
