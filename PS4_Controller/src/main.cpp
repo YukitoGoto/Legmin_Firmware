97c5d5481e9b2839221655fbb4e8b618bbef433c
yukito goto <ykt3693@outlook.jp>
2020-08-21 17:53:56 +0900

    update loop

--- PS4_Controller/src/main.cpp
+++ PS4_Controller/src/main.cpp
@@ -4,17 +4,30 @@
 //your esp32 bluetooth mac addr
 char esp32_btmac_addr[] = "30:ae:a4:08:01:90";
 
+//count
+uint32_t cnt;
+
+//time
+uint64_t pre_time;
+
 void setup() {
     Serial.begin(9600);
     PS4.begin(esp32_btmac_addr);
-    Serial.println("Initialization ready!");
+    cnt = 0;
+    pre_time = millis();
+    Serial.printf("\nInitialization ready!\n");
 }
 
 void loop() {
     if(PS4.isConnected()){
-        Serial.println("Controller connected!");
+        //Serial.println("Controller connected!");
+        cnt++;
     }else{
-        Serial.println("Searching Controller...");
+        //Serial.println("Searching Controller...");
     }
-    delay(1000);
+    if(millis() - pre_time > 3000){
+        pre_time = millis();
+        Serial.printf("Connected count:%d\n",cnt);
+        cnt = 0;
+    }
 }
