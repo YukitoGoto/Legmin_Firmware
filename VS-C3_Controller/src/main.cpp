6673ca8b240d8e409ecb4a3df12f6cfbc727225a
unknown <ykt3693@outlook.jp>
2020-10-02 07:03:20 +0900

    counldn't spi

--- VS-C3_Controller/src/main.cpp
+++ VS-C3_Controller/src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include <SPI.h>
+
+long pretime,pretime2;
+int send_data;
+uint8_t receive_data[8];
 
 void setup() {
-  // put your setup code here, to run once:
+  SPI.begin(SCK,MISO,MOSI,SS);  //VSPI - SCK,MISO,MOSI,SS = 18,19,23,5
+  SPI.beginTransaction(SPISettings(300000,MSBFIRST,SPI_MODE1));
+  Serial.begin(9600);
+  pretime2 = pretime = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(millis() - pretime2 > 150){
+    for(int i = 0;i < 8;i++)
+      receive_data[i] = SPI.transfer(send_data);
+    pretime2 = millis();
+  }
+  if(millis() - pretime > 1000){
+    Serial.printf("\nreceive_data:");
+    for(int i = 0;i < 8;i++)
+      Serial.printf(" %x",receive_data[i]);
+    pretime = millis();
+  }
+
 }
