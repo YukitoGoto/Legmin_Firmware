84bd7d6668a76f19087eb603dd025c4f4b899c73
unknown <ykt3693@outlook.jp>
2020-10-01 18:07:25 +0900

    add esp32_available_gpio.h

---
+++ VS-C3_Controller/lib/esp32_available_gpio.h
@@ -0,0 +1,17 @@
+//GPIOマトリクスで使用可能なピン一覧
+//https://trac.switch-science.com/wiki/esp32_tips#SPI%E9%80%9A%E4%BF%A1
+
+#define GPIO_NUM_4 4
+#define GPIO_NUM_5 5
+#define GPIO_NUM_13 13
+#define GPIO_NUM_14 14
+#define GPIO_NUM_16 16
+#define GPIO_NUM_17 17
+#define GPIO_NUM_18 18
+#define GPIO_NUM_19 19
+#define GPIO_NUM_21 21
+#define GPIO_NUM_22 22
+#define GPIO_NUM_23 23
+#define GPIO_NUM_25 25
+#define GPIO_NUM_26 26
+#define GPIO_NUM_27 27
